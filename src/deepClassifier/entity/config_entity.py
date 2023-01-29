62230885e0dc79ac20ffb22e3a044327003ccc53
Nitish Katkade <nitishkatkade94@gmail.com>
2023-01-29 14:03:18 +0530

    config_enitity added

--- src/deepClassifier/entity/config_entity.py
+++ src/deepClassifier/entity/config_entity.py
@@ -1,4 +1,4 @@
-from dataclasses.dataclass import dataclass
+from dataclasses import dataclass
 from pathlib import Path
 from deepClassifier.constants import *
 from deepClassifier.utils import read_yaml,create_directories
